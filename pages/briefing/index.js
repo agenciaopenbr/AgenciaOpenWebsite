76a397308f243a5705b7c44629d361c2e8ad1460
joaopedroalcn <joaopedro.ufma@gmail.com>
2022-04-01 13:35:32 -0300

    form com todos os campos obrigatorios

--- pages/briefing/index.js
+++ pages/briefing/index.js
@@ -15,17 +15,17 @@ export default function Briefing() {
         <form action="https://formsubmit.co/open.solucoes@outlook.com" className="contact-form" method="POST">
           <div className="form-briefing">
             <label htmlFor="nomeDaMarca">Qual nome será usado na marca? Haverá slogan?</label>
-            <textarea name="Qual nome será usado na marca? Haverá slogan?" className="place-text areaBriefing" id="nomeDaMarca"></textarea>
+            <textarea required name="Qual nome será usado na marca? Haverá slogan?" className="place-text areaBriefing" id="nomeDaMarca"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="referencia">Tem em mente alguma referência ou elemento específico para fazer parte do logotipo?</label>
-            <textarea name="Tem em mente alguma referência ou elemento específico para fazer parte do logotipo?" className="place-text areaBriefing" id="referencia"></textarea>
+            <textarea required name="Tem em mente alguma referência ou elemento específico para fazer parte do logotipo?" className="place-text areaBriefing" id="referencia"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="elemento">Existe algum elemento que você não gostaria que estivesse presente na criação?</label>
-            <textarea name="Existe algum elemento que você não gostaria que estivesse presente na criação?" className="place-text areaBriefing" id="elemento"></textarea>
+            <textarea required name="Existe algum elemento que você não gostaria que estivesse presente na criação?" className="place-text areaBriefing" id="elemento"></textarea>
           </div>
 
           <div className="form-tons">
@@ -48,28 +48,28 @@ export default function Briefing() {
           <div className="form-briefing">
             <label htmlFor="cores">Possui alguma preferência de cor? Se sim, qual(is)?
             </label>
-            <textarea name="Possui alguma preferência de cor? Se sim, qual(is)?
+            <textarea required name="Possui alguma preferência de cor? Se sim, qual(is)?
 " className="place-text areaBriefing" id="cores"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="adjetivos">Quais adjetivos você gostaria que melhor descrevessem sua futura marca?</label>
-            <textarea name="Quais adjetivos você gostaria que melhor descrevessem sua futura marca?" className="place-text areaBriefing" id="adjetivos"></textarea>
+            <textarea required name="Quais adjetivos você gostaria que melhor descrevessem sua futura marca?" className="place-text areaBriefing" id="adjetivos"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="concorrentes">Cite pelo menos 3 concorrentes seus e diga o que você acha sobre o logotipo que possuem.</label>
-            <textarea name="Cite pelo menos 3 concorrentes seus e diga o que você acha sobre o logotipo que possuem." className="place-text areaBriefing" id="concorrentes"></textarea>
+            <textarea required name="Cite pelo menos 3 concorrentes seus e diga o que você acha sobre o logotipo que possuem." className="place-text areaBriefing" id="concorrentes"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="tipografia">Que estilo de tipografia mais lhe agrada?</label>
-            <textarea name="Que estilo de tipografia mais lhe agrada?" placeholder="Exemplo : caligráfica , moderna , fontes finas, etc. " className="place-text areaBriefing" id="tipografia"></textarea>
+            <textarea required name="Que estilo de tipografia mais lhe agrada?" placeholder="Exemplo : caligráfica , moderna , fontes finas, etc. " className="place-text areaBriefing" id="tipografia"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="tipografia">Coloque aqui todas as informações de contato que deverão estar presentes em seus materiais de identidade visual.</label>
-            <textarea name="Coloque aqui todas as informações de contato que deverão estar presentes em seus materiais de identidade visual." placeholder="Informe seu número de contato , Whatsapp , e-mail, endereço, etc. " className="place-text areaBriefing" id="tipografia"></textarea>
+            <textarea required name="Coloque aqui todas as informações de contato que deverão estar presentes em seus materiais de identidade visual." placeholder="Informe seu número de contato , Whatsapp , e-mail, endereço, etc. " className="place-text areaBriefing" id="tipografia"></textarea>
           </div>
 
           <input type="submit" value="Enviar" className="submit submit-briefing" />
--- pages/briefingsite/index.js
+++ pages/briefingsite/index.js
@@ -15,29 +15,29 @@ export default function Briefing() {
         <form action="https://formsubmit.co/open.solucoes@outlook.com" className="contact-form" method="POST">
           <div className="form-briefing">
             <label htmlFor="nomeDaMarca">Nos informe as abas que deseja no site e os dados importantes para conterem em cada uma delas. Exemplo: Aba: Especialidades | Descrição: Otorrinolaringologia, Fonoaudiologia, etc.</label>
-            <textarea name="Nos informe as abas que deseja no site e os dados importantes para conterem em cada uma delas. Exemplo: Aba: Especialidades | Descrição: Otorrinolaringologia, Fonoaudiologia, etc." className="place-text areaBriefing" id="nomeDaMarca"></textarea>
+            <textarea required name="Nos informe as abas que deseja no site e os dados importantes para conterem em cada uma delas. Exemplo: Aba: Especialidades | Descrição: Otorrinolaringologia, Fonoaudiologia, etc." className="place-text areaBriefing" id="nomeDaMarca"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="referencia">Nos informe os dados de contato que devam constar no site, como telefones, whatsapp, endereços, redes sociais, etc.</label>
-            <textarea name="Nos informe os dados de contato que devam constar no site, como telefones, whatsapp, endereços, redes sociais, etc." className="place-text areaBriefing" id="referencia"></textarea>
+            <textarea required name="Nos informe os dados de contato que devam constar no site, como telefones, whatsapp, endereços, redes sociais, etc." className="place-text areaBriefing" id="referencia"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="elemento">No campo de formulário de agendamento de consultas, quais campos você deseja que o paciente preencha? Exemplo: Nome completo, WhatsApp, motivo da consulta, etc.</label>
-            <textarea name="No campo de formulário de agendamento de consultas, quais campos você deseja que o paciente preencha? Exemplo: Nome completo, WhatsApp, motivo da consulta, etc." className="place-text areaBriefing" id="elemento"></textarea>
+            <textarea required name="No campo de formulário de agendamento de consultas, quais campos você deseja que o paciente preencha? Exemplo: Nome completo, WhatsApp, motivo da consulta, etc." className="place-text areaBriefing" id="elemento"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="cores">Nos informe um e-mail para onde serão direcionados os formulários de agendamento dos seus pacientes. Toda solicitação de agendamento chegará no e-mail informado. 
             </label>
-            <textarea name="Nos informe um e-mail para onde serão direcionados os formulários de agendamento dos seus pacientes. Toda solicitação de agendamento chegará no e-mail informado. 
+            <textarea required name="Nos informe um e-mail para onde serão direcionados os formulários de agendamento dos seus pacientes. Toda solicitação de agendamento chegará no e-mail informado. 
 " className="place-text areaBriefing" id="cores"></textarea>
           </div>
 
           <div className="form-briefing">
             <label htmlFor="adjetivos">Há algum site de referência que você goste? Há alguma preferência no layout que você deseje nos informar?</label>
-            <textarea name="Há algum site de referência que você goste? Há alguma preferência no layout que você deseje nos informar?" className="place-text areaBriefing" id="adjetivos"></textarea>
+            <textarea required name="Há algum site de referência que você goste? Há alguma preferência no layout que você deseje nos informar?" className="place-text areaBriefing" id="adjetivos"></textarea>
           </div>
 
           <input type="submit" value="Enviar" className="submit submit-briefing" />
